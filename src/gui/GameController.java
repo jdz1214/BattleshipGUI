1b6c64a7b26a6e775a9324a10fb9edfcd27423fc
jdz1214 <duncan@atlasinnovation.io>
2016-07-12 08:48:24 -0400

    creating game controller class

---
+++ src/gui/GameController.java
@@ -0,0 +1,18 @@
+package gui;
+
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+/**
+ * Created by jdz on 7/12/16.
+ */
+public class GameController implements Initializable {
+    @FXML 
+
+
+    @Override
+    public void initialize(URL location, ResourceBundle resources) {}
+}

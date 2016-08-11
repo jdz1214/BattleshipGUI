e8fd47a5112eb3dfd2ae4dd847572cc0f15c81cd
Duncan Zahringer <duncan@atlasinnovation.io>
2016-08-10 21:58:28 -0400

    Changed game layout to use buttons.

--- src/client/GameController.java
+++ src/client/GameController.java
@@ -1,12 +1,10 @@
 package client;
 
-import commoncore.Game;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.*;
 import javafx.scene.layout.GridPane;
-import javafx.scene.text.Text;
 
 import java.net.URL;
 import java.util.ResourceBundle;
@@ -18,46 +16,46 @@ public class GameController implements Initializable {
     @FXML private Label lblYourFleet;
     @FXML private GridPane gridAttackHistory;
     @FXML private GridPane gridFleet;
-    @FXML private Button btnQuit;
-    @FXML private Button btnAttack;
     @FXML private TextArea txtGameChat;
     @FXML private TextField txtInput;
-    @FXML private Text gridHistory00;
-    @FXML private Text gridHistory01;
-    @FXML private Text gridHistory02;
-    @FXML private Text gridHistory03;
-    @FXML private Text gridHistory04;
-    @FXML private Text gridHistory05;
-    @FXML private Text gridHistory10;
-    @FXML private Text gridHistory11;
-    @FXML private Text gridHistory12;
-    @FXML private Text gridHistory13;
-    @FXML private Text gridHistory14;
-    @FXML private Text gridHistory15;
-    @FXML private Text gridHistory20;
-    @FXML private Text gridHistory21;
-    @FXML private Text gridHistory22;
-    @FXML private Text gridHistory23;
-    @FXML private Text gridHistory24;
-    @FXML private Text gridHistory25;
-    @FXML private Text gridHistory30;
-    @FXML private Text gridHistory31;
-    @FXML private Text gridHistory32;
-    @FXML private Text gridHistory33;
-    @FXML private Text gridHistory34;
-    @FXML private Text gridHistory35;
-    @FXML private Text gridHistory40;
-    @FXML private Text gridHistory41;
-    @FXML private Text gridHistory42;
-    @FXML private Text gridHistory43;
-    @FXML private Text gridHistory44;
-    @FXML private Text gridHistory45;
-    @FXML private Text gridHistory50;
-    @FXML private Text gridHistory51;
-    @FXML private Text gridHistory52;
-    @FXML private Text gridHistory53;
-    @FXML private Text gridHistory54;
-    @FXML private Text gridHistory55;
+    @FXML private Button btnQuit;
+    @FXML private Button btnAttack;
+    @FXML private Button gridHistory00;
+    @FXML private Button gridHistory01;
+    @FXML private Button gridHistory02;
+    @FXML private Button gridHistory03;
+    @FXML private Button gridHistory04;
+    @FXML private Button gridHistory05;
+    @FXML private Button gridHistory10;
+    @FXML private Button gridHistory11;
+    @FXML private Button gridHistory12;
+    @FXML private Button gridHistory13;
+    @FXML private Button gridHistory14;
+    @FXML private Button gridHistory15;
+    @FXML private Button gridHistory20;
+    @FXML private Button gridHistory21;
+    @FXML private Button gridHistory22;
+    @FXML private Button gridHistory23;
+    @FXML private Button gridHistory24;
+    @FXML private Button gridHistory25;
+    @FXML private Button gridHistory30;
+    @FXML private Button gridHistory31;
+    @FXML private Button gridHistory32;
+    @FXML private Button gridHistory33;
+    @FXML private Button gridHistory34;
+    @FXML private Button gridHistory35;
+    @FXML private Button gridHistory40;
+    @FXML private Button gridHistory41;
+    @FXML private Button gridHistory42;
+    @FXML private Button gridHistory43;
+    @FXML private Button gridHistory44;
+    @FXML private Button gridHistory45;
+    @FXML private Button gridHistory50;
+    @FXML private Button gridHistory51;
+    @FXML private Button gridHistory52;
+    @FXML private Button gridHistory53;
+    @FXML private Button gridHistory54;
+    @FXML private Button gridHistory55;
     private Main m;
     private String opponentUsername;
 
@@ -67,12 +65,13 @@ public class GameController implements Initializable {
         m.showGUI();
     }
 
-    @FXML private void onEnter() {
+    @FXML
+    private void onEnter() {
         sendMessage();
     }
 
     @FXML
-    public void init(Main m, String opponentUsername) {
+    void init(Main m, String opponentUsername) {
         this.m = m;
         titledPane.setText("Battleship - Game: " + m.getUsername());
         lblAttackHistory.setText("Attack History vs. " + opponentUsername);
@@ -84,7 +83,9 @@ public class GameController implements Initializable {
         btnAttack.setDisable(false);
     }
 
-    public void updateHistory(Game.History history) {
+    public void updateHistory(ActionEvent event) {
+        Button btn = (Button) event.getSource();
+        String btnName = btn.getId();
         //Todo
     }
 
@@ -98,12 +99,12 @@ public class GameController implements Initializable {
     }
 
     @FXML
-    public void updateChat(String msg) {
+    void updateChat(String msg) {
         txtGameChat.appendText(msg + "\n");
     }
 
     @FXML
-    public void disableChat() {
+    void disableChat() {
         txtInput.setEditable(false);
         txtInput.setDisable(true);
     }
